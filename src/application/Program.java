88fcda9100797a7f66b729b3f309a1fc5ac6053f
Guilherme <guilherme.g.andrade11@gmail.com>
2023-10-02 19:15:15 -0300

    Camada Chess e imprimindo o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        Position posicao = new Position(3, 5);
-        System.out.println(posicao);
+        
+    	ChessMatch chessMatch = new ChessMatch();
+    	UI.printBoard(chessMatch.getPieces());
+    	
     }
 }
